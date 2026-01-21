7fd2ace7a8264c2981084f1cfeb6965f1ca7b541
Gabriel <ghsoares99795@gmail.com>
2026-01-21 12:23:36 -0300

    Fix for github pages

--- docs/custom.py
+++ docs/custom.py
@@ -1,10 +1,12 @@
+import os
 
 def configure_pages(gen):
 	index_page = gen.get_template('generic.html')
 	gen.add_page({
 		"name": "Godot RmlUi",
-		"href": "/index.html",
+		"href": "./index.html",
 		"location": "/",
+		"filename": gen.dist_path("index.html"),
 		"content": index_page
 	})
 
